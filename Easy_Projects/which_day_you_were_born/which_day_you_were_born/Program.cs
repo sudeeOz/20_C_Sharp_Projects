95c6c5ce2a7d5b3404c41b553957c169acd1cfa5
Sude Nur Özkan <1030521431@erciyes.edu.tr>
2025-08-14 23:49:29 +0300

    Are you wondering what day you were born?

---
+++ Easy_Projects/which_day_you_were_born/which_day_you_were_born/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace which_day_you_were_born
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hello, his is a simple program to find out which day of the week you were born on.
+            Console.WriteLine("Enter your date of birth (dd/mm/yyyy):");
+            string input = Console.ReadLine();
+            DateTime dateOfBirth;
+            // Try to parse the input date
+            if (DateTime.TryParse(input, out dateOfBirth))
+            {
+                // Get the day of the week
+                DayOfWeek dayOfWeek = dateOfBirth.DayOfWeek;
+                // Output the result
+                Console.WriteLine($"You were born on a {dayOfWeek}.");
+            }
+            else
+            {
+                // If parsing fails, inform the user
+                Console.WriteLine("Invalid date format. Please enter the date in dd/mm/yyyy format.");
+            }
+        }
+    }
+}
