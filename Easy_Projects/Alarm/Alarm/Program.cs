71d9b26e0c75707e20680d1c192a5ce1152085ff
Sude Nur Özkan <1030521431@erciyes.edu.tr>
2025-08-15 00:00:20 +0300

    Set the Alarm!

---
+++ Easy_Projects/Alarm/Alarm/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alarm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Should you set the alarm? You are in right place.
+            Console.WriteLine("Welcome to the Alarm System!");
+            Console.WriteLine("Please enter the time to set the alarm (HH:mm):");
+            string inputTime = Console.ReadLine();
+            DateTime alarmTime;
+            if (DateTime.TryParse(inputTime, out alarmTime))
+            {
+                Console.WriteLine($"Alarm set for {alarmTime.ToShortTimeString()}");
+                while (true)
+                {
+                    if (DateTime.Now >= alarmTime)
+                    {
+                        Console.WriteLine("Alarm ringing! Time to wake up!");
+                        Console.Beep(); // This will make a beep sound
+                        break;
+                    }
+                    System.Threading.Thread.Sleep(1000); // Check every second
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid time format. Please use HH:mm.");
+            }
+        }
+    }
+}
