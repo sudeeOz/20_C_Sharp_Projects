c446723edd2ccdbcd6edc6de7a3cd132b0128061
Sude Nur Özkan <1030521431@erciyes.edu.tr>
2025-09-21 21:12:04 +0300

    ToDoList

---
+++ Mid-Level_Projects/ToDo_ListApp/ToDo_List/ToDo_List/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDo_List
+{
+    public partial class Form1 : Form
+    {
+        List<Task> tasks = new List<Task>();
+        public Form1()
+        {
+           
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task newTask = new Task
+            {
+                Title = textBox1.Text,
+                DueTime = dateTimePicker1.Value,
+                IsCompleted = false,
+            };
+            tasks.Add(newTask);
+            checkedListBox1.Items.Add(newTask, false);
+
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.SelectedItem is Task selectedTask)
+            {
+                tasks.Remove(selectedTask);
+                checkedListBox1.Items.Remove(selectedTask);
+            }
+            else
+            {
+                MessageBox.Show("Lütfen silmek için bir görev seçiniz!");
+            }
+        }
+
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            Task selectedTask = (Task)checkedListBox1.Items[e.Index];
+
+            if (e.NewValue == CheckState.Checked)
+                selectedTask.IsCompleted = true;
+            else
+                selectedTask.IsCompleted = false;
+
+            checkedListBox1.Items[e.Index] = selectedTask;
+        }
+    }
+    public class Task
+    {
+        public string Title { get; set; }
+        public DateTime DueTime { get; set; }
+        public bool IsCompleted { get; set; }
+        
+        public override string ToString()
+        {
+            return $"{Title} - Tarih: {DueTime.ToShortTimeString()} - : {(IsCompleted ? "Tamamlandı" : "Beklemede" )}";
+        }
+    } 
+}
---
+++ Mid-Level_Projects/ToDo_ListApp/ToDo_List/ToDo_List/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ToDo_List")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ToDo_List")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("04738232-c494-4f5f-9db8-5638bf1647fe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
